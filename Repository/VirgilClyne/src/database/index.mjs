235af3e5df69ae90a19c920a8eb9d8c90ba95b11
GitHub Action <action@github.com>
2024-10-06 08:40:56 +0800

    2024-10-06 08:40

--- Repository/VirgilClyne/src/database/index.mjs
+++ Repository/VirgilClyne/src/database/index.mjs
@@ -1,17 +1,7 @@
 import * as Default from "./Default.json";
-import * as Location from "./Location.json";
-import * as Maps from "./Maps.json";
-import * as News from "./News.json";
 import * as PrivateRelay from "./PrivateRelay.json";
-import * as TestFlight from "./TestFlight.json";
-import * as TV from "./TV.json";
 
 export default Database = {
 	"Default": Default,
-	"Location": Location,
-	"Maps": Maps,
-	"News": News,
-	"PrivateRelay": PrivateRelay,
-	"TestFlight": TestFlight,
-	"TV": TV
+	"PrivateRelay": PrivateRelay
 };
