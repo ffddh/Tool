20e603a4656e00e9a6184b519555d8d2388e6968
GitHub Action <action@github.com>
2024-11-25 08:42:44 +0800

    2024-11-25 08:42

--- Repository/Repcz/Clash/Meta/Override.js
+++ Repository/Repcz/Clash/Meta/Override.js
@@ -2,7 +2,7 @@
 //
 // Clash Verge Rev (Version ≥ 17.2) & Mihomo-Party (Version ≥ 1.5.10)
 //
-// 最后更新时间: 2024-11-24 21:00
+// 最后更新时间: 2024-11-25 07:15
 
 // 规则集通用配置
 const ruleProviderCommon = {
@@ -163,6 +163,14 @@ function main(config) {
       "include-all": true,
       "proxies": ["手动切换", "香港节点", "美国节点", "狮城节点", "日本节点", "台湾节点", "DIRECT"],
       "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Emby.png"
+    },
+    {
+      ...groupBaseOption,
+      "name": "Spotify",
+      "type": "select",
+      "include-all": true,
+      "proxies": ["手动切换", "香港节点", "美国节点", "狮城节点", "日本节点", "台湾节点", "DIRECT"],
+      "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Spotify.png"
     },
     {
       ...groupBaseOption,
@@ -360,7 +368,7 @@ function main(config) {
     "RULE-SET,Steam,游戏平台",
     "RULE-SET,Epic,游戏平台",
     "RULE-SET,Emby,Emby",
-    "RULE-SET,Spotify,国际媒体",
+    "RULE-SET,Spotify,Spotify",
     "RULE-SET,Bahamut,国际媒体",
     "RULE-SET,Netflix,国际媒体",
     "RULE-SET,Disney,国际媒体",
